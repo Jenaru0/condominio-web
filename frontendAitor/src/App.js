26216d51aeaac6079ca5ac3ff0dff867ce6e200b
Aitor <2201010047@undc.edu.pe>
2024-12-02 00:42:35 -0500

    Initial commit - FrontEnd Setup

--- frontendAitor/src/App.js
+++
@@ -1,9 +0,0 @@
-// src/App.js
-import React from 'react';
-import AppAdmin from './administrador/AppAdmin';  // Ajusta esta ruta si es necesario
-
-const App = () => {
-    return <AppAdmin />;
-};
-
-export default App;
---
+++ frontendAitor/src/usuarios/administrador/paginas/Propiedades/index.js
@@ -0,0 +1,12 @@
+// src/usuarios/administrador/paginas/Propiedades/index.js
+import React from 'react';
+
+const Propiedades = () => {
+    return (
+        <div>
+            <h1>Gestión de Propiedades</h1>
+        </div>
+    );
+};
+
+export default Propiedades;
